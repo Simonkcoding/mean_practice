df22830096fb4558cb16934e17692d49c3827ca0
simonkcoding <ykwok3@uncc.edu>
2018-12-18 19:43:52 -0500

    123

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms'
+
+import {
+  MatToolbarModule,
+  MatInputModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatTableModule,
+  MatDividerModule,
+  MatSnackBarModule,
+  MatFormFieldModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+
+import { IssueService } from './issue.service'
+
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'edit/:id', component: EditComponent },
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    ReactiveFormsModule
+  ],
+  providers: [IssueService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
